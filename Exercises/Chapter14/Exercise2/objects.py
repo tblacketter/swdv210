2e086c3d1a4189d8ee818a513ff03346e45a2255
tblacketter <130483945+tblacketter@users.noreply.github.com>
2024-03-11 07:59:11 -0600

    asd

---
+++ Exercises/Chapter14/Exercise2/objects.py
@@ -0,0 +1,14 @@
+from dataclasses import dataclass
+
+@dataclass
+class Movie:
+    name: str
+    year: int
+
+    def __init__(self, nameInit, yearInit):
+        self.name = nameInit
+        self.year = yearInit
+
+    def getStr(self):
+        print(f"{self.name} ({self.year})")
+
